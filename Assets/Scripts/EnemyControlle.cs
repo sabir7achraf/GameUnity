b51aa5335666ec16ae32ccb7c6124bc59f0aa687
sabir7achraf <146297826+sabir7achraf@users.noreply.github.com>
2024-05-18 22:17:28 +0100

    EnemyColliderWithUnwalkAbleLayer

---
+++ Assets/Scripts/EnemyControlle.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+   // Public variables
+   public float speed;
+   public bool vertical;
+   public float changeTime = 3.0f;
+   public LayerMask unwalkableMask;
+
+   // Private variables
+   Rigidbody2D rigidbody2d;
+   float timer;
+   int direction = 1;
+
+   // Reference to GridA
+   GridA grid;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+       rigidbody2d = GetComponent<Rigidbody2D>();
+       grid = FindObjectOfType<GridA>();
+       timer = changeTime;
+   }
+
+   // Update is called every frame
+   void Update()
+   {
+       timer -= Time.deltaTime;
+
+       if (timer < 0)
+       {
+           direction = -direction;
+           timer = changeTime;
+       }
+   }
+
+   // FixedUpdate has the same call rate as the physics system
+   void FixedUpdate()
+   {
+       Vector2 position = rigidbody2d.position;
+
+       if (vertical)
+       {
+           position.y = position.y + speed * direction * Time.deltaTime;
+       }
+       else
+       {
+           position.x = position.x + speed * direction * Time.deltaTime;
+       }
+
+       // Check if the new position is in a non-walkable area
+       if (IsPositionWalkable(position))
+       {
+           rigidbody2d.MovePosition(position);
+       }
+       else
+       {
+           direction = -direction; // Reverse direction if the new position is not walkable
+       }
+   }
+
+   bool IsPositionWalkable(Vector2 position)
+   {
+       Collider2D hitCollider = Physics2D.OverlapCircle(position, grid.nodeRadius, unwalkableMask);
+       return hitCollider == null;
+   }
+}
